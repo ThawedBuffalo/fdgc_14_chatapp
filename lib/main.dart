85549abf0b68650c733db24dd7fb1c1c620619e4
JC <thawedbuffalo@gmail.com>
2024-11-10 06:58:43 -0500

    section 14- chapter 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fgdc_14_chatapp/screens/auth.dart';
 import 'package:fgdc_14_chatapp/screens/chat.dart';
+import 'package:fgdc_14_chatapp/screens/splash.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -30,6 +31,10 @@ class App extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+
           if (snapshot.hasData) {
             return const ChatScreen();
           }
