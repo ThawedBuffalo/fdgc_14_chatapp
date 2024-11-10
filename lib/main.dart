642141c7bca8d982164109b70e4c7ec835bf5f85
JC <thawedbuffalo@gmail.com>
2024-11-10 06:46:35 -0500

    section 14- chapter 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:fgdc_14_chatapp/screens/auth.dart';
+import 'package:fgdc_14_chatapp/screens/chat.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -6,8 +8,10 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //await Firebase.initializeApp(options: const FirebaseOptions(
-      //apiKey: '', appId: '', messagingSenderId: '',projectId:  ''));
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  //apiKey: '', appId: '', messagingSenderId: '',projectId:  ''));
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
@@ -17,12 +21,22 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'FlutterChat',
-        theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 63, 17, 177)),
-        ),
-        home: const AuthScreen());
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+
+          return const AuthScreen();
+        },
+      ),
+    );
   }
 }
